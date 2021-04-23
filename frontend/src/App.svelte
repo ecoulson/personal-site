120ef75274e80743262eabf36d538c605a0e50fb
Evan Coulson <evan.m.coulson@gmail.com>
2021-04-22 17:50:29 -0700

    inital commit

---
+++ frontend/src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Navbar from "./Navbar.svelte";
+	import Home from "./Home.svelte";
+	import { Router, Route, Link } from "svelte-routing";
+</script>
+
+<Router>
+	<Navbar />
+	<Route path="/">
+		<Home />
+	</Route>
+</Router>
+
+<style>
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		text-align: center;
+	}
+</style>
---
+++ frontend/src/Navbar.svelte
@@ -0,0 +1,39 @@
+<script lang="ts"> 
+    import { links } from "svelte-routing";
+</script>
+
+<nav use:links>
+    <div class="nav-group main-group">
+        <a href="/">Evan Coulson</a>
+    </div>
+    <div class="nav-group page-group">
+        <a href="/about">About</a>
+        <a href="/projects">Projects</a>
+    </div>
+</nav>
+
+<style>
+    nav {
+        box-sizing: border-box;
+        width: 100%;
+        height: 75px;
+        padding: 15px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #FAAA8D;
+    }
+    .nav-group {
+        display: flex;
+    }
+    .main-group {
+        font-size: 28px;
+    }
+    .page-group {
+        font-size: 24px;
+    }
+    a {
+        color: #201E1F;
+        margin: 0 25px;
+    }
+</style>
